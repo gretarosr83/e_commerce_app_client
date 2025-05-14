838fa865feb0fc0509b9579dac0eb32039bc7d06
Greta Ros <121072663+gretarosr83@users.noreply.github.com>
2025-05-14 04:52:38 -0500

    backend.js updated

--- src/constants/backend.js
+++ src/constants/backend.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL_ENDPOINT = 'http://localhost:8080/api/v1/'
+export const BACKEND_URL_ENDPOINT = 'http://34.234.93.221:8080/api/v1/'
 
 export const GET_ALL_PRODUCTS_API = BACKEND_URL_ENDPOINT + 'products'
 
